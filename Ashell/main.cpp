9429a9ac1489dab5145cdbd8e0a059946b018f80
wirdal <wirdal@ad3.ucdavis.edu@pc10.cs.ucdavis.edu>
2018-10-03 20:18:16 -0700

    Mainloop psuedo added

--- Ashell/main.cpp
+++ Ashell/main.cpp
@@ -1,4 +1,10 @@
 #include "Ashell.hh"
 int main (int argc, char *argv[]) {
   //Main loops goes here
+  //While true
+    //Wait for input
+      //on input
+      //Parse commands
+        //do appropriate actions
+        //return on input
  }
