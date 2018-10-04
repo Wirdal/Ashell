dfa98ab2beafceb4d6469caf567614e170b06331
bllarrison <35443795+bllarrison@users.noreply.github.com>
2018-10-03 20:57:20 -0700

    Update parse_test.cpp
    splits commands and variables

--- parse_test.cpp
+++ parse_test.cpp
@@ -7,24 +7,47 @@ int main (int argc, char *argv[]) {
 
     int i = 0;
     
-    char input[] = "cd example";
-    std::cout << "Command typed: " << input << "\n";
-    
-    char command[2];
-    char argument[10];
+    char input[] = "cd home/go";
+    //std::cout << "Command typed: " << input << "\n";
+    int space_loc = 0;
+	int num_spaces = 0;
+    char command[5];
+    char argument[514];
     
     while(input[i] != '\0'){
+	//If a space is found
         if(input[i] == ' '){
-            for(int a = 0; a < i; ++a){
-                command[a] = input[a];
-            }
-            std::cout << "command: "<< command << "\n";
+	  	num_spaces++;
+	   	space_loc=i;
+
+		//if this is the first space found
+	   	if(num_spaces == 1){
+		    for(int a = 0; a < i; ++a){
+		        command[a] = input[a];
+			std::cout << "put in command: "<< input[a] << "\n";
+		    }
+		command[i] = '\0'; 
+	}
+	
+	}
+	if(input[i+1] == '\0'){
+		for(int a = 0; a < i - space_loc; ++a){
+			argument[a] = input[a + space_loc + 1];
+			argument[a + 1] = '\0';
+			
+		}
+	argument[i+1] = '\0';
+
         }
-        
-        std::cout << input[i]<<" " << i << "\n";
+
+	std::cout << "input at i:  "<< input[i] << "\n";
         i ++;
+	
         
     }
+
+	std::cout << "Command: "<< command << "\n";
+	std::cout << "Arg: "<< argument << "\n";
     
     
 }
