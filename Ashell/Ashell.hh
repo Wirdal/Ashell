ab98c77c9b6aa280d8d6c5198e1f8a18d7637457
wirdal <wirdal@ad3.ucdavis.edu@pc10.cs.ucdavis.edu>
2018-10-03 19:23:49 -0700

    Changed headers in AShell

--- Ashell/Ashell.hh
+++ Ashell/Ashell.hh
@@ -8,14 +8,15 @@ Probaby will be more here, but define implemetnation here first
 
 
 // I/O services
+void AshellPrint(const char* output);
+void AshellPrint(std::string output);
+void AshellPrint(int output);
 
 //Path: Name of the file to open. It includes the file path defining where to open or create a file
 //Flag: The permissiosn given to us by the file; performed by bitwise OR
 //Returns: A handle that we use to refer to that file
 int AshellOpen(const char *path, int flags, .../*, mode_t mode*/);
 
-//
-void AshellPrint(const char* output);
 
 //FD, is the handle returned by open
 //Buf, the buffer where the read data is stored to
--- Ashell/main.cpp
+++ Ashell/main.cpp
@@ -1,4 +1,4 @@
-#include "Ashell.h"
+#include "Ashell.hh"
 int main (int argc, char *argv[]) {
   //Main loops goes here
  }
