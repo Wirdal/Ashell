e51c255c74d3e24b1596f962a6b3c267096e2f29
bllarrison <35443795+bllarrison@users.noreply.github.com>
2018-10-04 22:54:59 -0700

    Add files via upload
    reads from command line, puts input into char prog[]

--- bens_playground/main.cpp
+++ bens_playground/main.cpp
@@ -3,32 +3,46 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    int max_size = 10;
+
+    bool end_line = false;
+    int max_size = 512;  //TODO: switch to buffer or malloc system if necessary
     int i = 0;
-    char prog = NULL;
-    char prog_print = NULL;
-    char arg = NULL;
 
 
+    size_t bytes_read = 0;
 
-    while (i < max_size){
+    char prog_print = NULL;\
+    char prog[max_size];
+    char ** args = NULL; //points to a set of pointers of char arrays
+    char command[10];
 
-        size_t bytes_read = read(0, &prog, 10);
-        //read(0, &prog_print, 1);
-        std::cout <<"bytes_read: " << bytes_read << "\n";
-        if (isprint(prog)){
-            //std::cout <<"this comes out: " << prog_print << "\n";
+    int max_bytes = 1; //reads at most 1 byte at a time
+    int fd_read = 0; //this if the fd (file descriptior) for read
 
+    while (!end_line){
+        //bytes_read is the number of bytes SUCCESSFULLY read
+        //
+        bytes_read = read(fd_read, &prog_print, max_bytes);
+
+        if (isprint(prog_print)){
+            std::cout <<"this comes out: " << prog_print << "\n";
+            prog[i] = prog_print;
         }
+
         else if ("0\\") {
-            std::cout << "now at the end of the string \n";
-
+            end_line = true;
+            prog[i] = '\0';
+            //std::cout << "now at the end of the string \n";
         }
 
+
+        //std::cout <<"i: " << i << "\n";
+
+        i++;
 
     }
-    //const char *input = "hello world";
-    //std::string input;
+    std::cout <<"prog:  " << prog << "\n";
+
 
 
 }
