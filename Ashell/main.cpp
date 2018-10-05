2cda93bc5eca861e82139bd0f9f9ae5e0c141db0
bllarrison <35443795+bllarrison@users.noreply.github.com>
2018-10-05 15:32:05 -0700

    Added ReadAndParseCmd();

--- Ashell/main.cpp
+++ Ashell/main.cpp
@@ -1,6 +1,8 @@
 #include "Ashell.hh"
 #include <unistd.h>
 int main (int argc, char *argv[]) {
+  
+  ReadAndParseCmd(); //eventually this will be main loop initiator: like while(ReadAndParseCmd()) [see pg 112 of text]
   //Main loops goes here
   //While true
     //Wait for input
