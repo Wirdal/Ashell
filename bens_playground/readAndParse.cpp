d81e9276b46657d35e09174ed28b380e530f45c1
bllarrison <35443795+bllarrison@users.noreply.github.com>
2018-10-05 15:39:06 -0700

    Update readAndParse.cpp

--- bens_playground/readAndParse.cpp
+++ bens_playground/readAndParse.cpp
@@ -4,52 +4,116 @@
 #include <stdio.h>
 #include <string.h>
 
+void AshellPrint(const char* output){
+//Write to 0 for STDIN_FILENO
+//Write to 1 fir STDOUT_FILENO
+//Write to 2 for STDERR_FILENO
+//We're gonna write to 1, so we can print to the console
+    //Find how much we need to write
+    int size = strlen(output);
+    int written = 0;
+    while(size != written){
+        int current = write(1, output, 1);
+        written = written + current;
+        output = output + current;
+    };
+};
+void AshellPrint(std::string output){
+    //I can't think of a better way to do this
+    AshellPrint(output.c_str());
+};
+void AshellPrint(int output){
+    std::string out2 = std::to_string(output);
+    AshellPrint(out2.c_str());
+};
 
+
+size_t AshellRead(int fd, void *buf, size_t count){
+//TODO
+};
+
+int AshellOpen(const char *path, int flags, .../*, mode_t mode*/){
+//TODO
+};
+// Shell commands
+
+
+void pwd(){
+    char *path = "home";
+    AshellPrint(path);
+    free(path);
+    AshellPrint("\n");
+};
+
+void exit(){
+    exit(EXIT_SUCCESS);
+};
+
+void cd(const char* directory){
+    int err = chdir(directory);
+    if (-1 == err){
+        AshellPrint("Error changing directory \n");
+    }
+};
+void cd(std::string directory){
+    const char* cdirectory = directory.c_str();
+    int err = chdir(cdirectory);
+    if (-1 == err){
+        AshellPrint("Error changing directory \n");
+
+    }
+};
+char* ls(char* directory){
+//TODO
+};
+
+char* ff(char* filename){
+//TODO
+};
 
 int size_of(char *array){
     int i = 0;
-
     while(array[i] != '\0'){
         i++;
     }
-
     return i;
-
 }
 
 void CallPrograms(char **seperated, int num_args){
-
     char * run_program = seperated[0];
-    //std::cout <<"Call Program " << run_program<< " with " << run_program[2] << " arguments"<<"\n";
-
-
 
     if(run_program[0] == 'c' && run_program[1] == 'd' && run_program[2] == '\0'){
         std::cout <<"Execute CD "<<"\n";
+        char * directory = seperated[1]; //TODO: check to see if it exists
+        cd(directory);
     }
     else if(run_program[0] == 'l' && run_program[1] == 's' && run_program[2] == '\0'){
         std::cout <<"Execute LS "<<"\n";
+        //TODO: ls(directory);
     }
     else if(run_program[0] == 'p' && run_program[1] == 'w' && run_program[2] == 'd' && run_program[3] == '\0'){
         std::cout <<"Execute PWD "<<"\n";
+        pwd();
     }
     else if(run_program[0] == 'e' && run_program[1] == 'x' && run_program[2] == 'i' && run_program[3] == 't' && run_program[4] == '\0'){
         std::cout <<"Execute EXIT "<<"\n";
+        exit();
     }
     else if(run_program[0] == 'f' && run_program[1] == 'f' && run_program[2] == '\0'){
         std::cout <<"Execute ff "<<"\n";
         char * filename = seperated[1];
         std::cout <<"Filename:  "<< filename <<"\n";
+        //TODO: ff(filename);
     }
     else{
         std::cout <<"Run Exec(" << run_program<<");" <<"\n";
+        //TODO: exec(program_name)
     }
 
 
 }
 
 void parse(char *prog, char **parsed){
-    //std::cout <<"got to parse" << "\n";
     //Parsing char array received, basically a split line function.
     //Currently seperates with ' ' TODO work with any character
 
@@ -77,24 +141,12 @@ void parse(char *prog, char **parsed){
     char * run_program = seperated[0];
     int num_args = i -1;
 
-    //std::cout <<"Run Program: " << run_program<<"\n";
-    //std::cout <<"Number of Args: " << num_args<<"\n";
-
 
     CallPrograms(seperated, num_args);
-
-    //SWITCH run_program == "cd"
-    //SWITCH run_program == "ls"
-    //SWITCH run_program == "pwd"
-
-
 }
 
-
-
-
 void ReadAndParseCmd() {
-    //std::cout <<"got to run" << "\n";
+
     bool end_line = false;
     int max_size = 512;  //TODO: switch to buffer or malloc system if necessary
     int i = 0;
@@ -142,9 +194,6 @@ void ReadAndParseCmd() {
 
         }
         //std::cout <<"out of if" << "\n";
-
-
-
         i++;
 
     }
@@ -153,11 +202,7 @@ void ReadAndParseCmd() {
     //std::cout <<"Done."<< "\n";
 
 
-
-
-
 }
-
 int main(int argc, char *argv[]) {
     ReadAndParseCmd();
 }
