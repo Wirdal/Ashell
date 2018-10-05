5bc3d3f5508cfa69f855079de2a3c2e12712ec5c
Chase Maguire <cmaguire@ucdavis.edu>
2018-10-04 18:02:46 -0700

    cd tests

--- sandbox/tests.cpp
+++ sandbox/tests.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
+#include <string>
 #include <cstring>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <typeinfo>
-#include <string>
-
-
 using namespace std;
-void AshellPrint(const char* output){
-// As of now, we can only print out strings
-//Write to 0 for STDIN_FILENO
-//Write to 1 fir STDOUT_FILENO
-//Write to 2 for STDERR_FILENO
-//We're gonna write to 1, so we can print to the console
-    //Find how much we need to write
-	int size = strlen(output);
-    int written = 0;
-    while(size != written){
-		int current = write(1, output, 1);
-        written = written + current;
-		output = output + current;
-    };
+void change(std::string directory){
+    const char* cdirectory = directory.c_str();
+    int err = chdir(cdirectory);
+	if (-1 == err){
+    	cout << "error" << "\n";
+    }
 };
 int main (int argc, char *argv[]) {
-	char* out;
-	getcwd(out, 0);
+	string path = "gibbersh";
+	change(path);
+	cout << get_current_dir_name() << "\n";
 }
