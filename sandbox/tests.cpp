4b63768a73aef05c04e59a1e5095fa9fabc38485
Chase Maguire <cmaguire@ucdavis.edu>
2018-10-07 12:57:26 -0700

    tests for group perms

--- sandbox/tests.cpp
+++ sandbox/tests.cpp
@@ -1,19 +1,31 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <iostream>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 using namespace std;
-void change(std::string directory){
-    const char* cdirectory = directory.c_str();
-    int err = chdir(cdirectory);
-	if (-1 == err){
-    	cout << "error" << "\n";
-    }
+void ls(){
+  DIR* dir;
+  struct dirent *entry;
+  dir = opendir(get_current_dir_name());
+  if (NULL != dir){
+	entry = readdir(dir);
+	// Set the buffer for stat here
+	struct stat *statbuff; //I think segfaults happen here?
+	while (NULL != entry){
+		stat(entry->d_name, statbuff);
+		__mode_t modes= statbuff->st_mode;
+		std::cout << modes << "\n";
+		printf(entry->d_name);
+		printf("\n");
+		entry = readdir(dir);
+
+	}
+  }
 };
 int main (int argc, char *argv[]) {
-	string path = "gibbersh";
-	change(path);
-	cout << get_current_dir_name() << "\n";
+	ls()
 }
