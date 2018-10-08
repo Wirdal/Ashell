5d9fb94cdf08c4061cd79dce380fa529183ce995
bllarrison <35443795+bllarrison@users.noreply.github.com>
2018-10-07 23:29:15 -0700

    Added non-canonical mode support

--- Ashell/Ashell.cpp
+++ Ashell/Ashell.cpp
@@ -7,6 +7,38 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+//from noncanmode.c
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <ctype.h>
+
+//from noncanmode.c
+void ResetCanonicalMode(int fd, struct termios *savedattributes){
+    tcsetattr(fd, TCSANOW, savedattributes);
+}
+
+//from noncanmode.c
+void SetNonCanonicalMode(int fd, struct termios *savedattributes){
+    struct termios TermAttributes;
+    char *name;
+
+    // Make sure stdin is a terminal.
+    if(!isatty(fd)){
+        fprintf (stderr, "Not a terminal.\n");
+        exit(0);
+    }
+
+    // Save the terminal attributes so we can restore them later.
+    tcgetattr(fd, savedattributes);
+
+    // Set the funny terminal modes.
+    tcgetattr (fd, &TermAttributes);
+    TermAttributes.c_lflag &= ~(ICANON | ECHO); // Clear ICANON and ECHO.
+    TermAttributes.c_cc[VMIN] = 1;
+    TermAttributes.c_cc[VTIME] = 0;
+    tcsetattr(fd, TCSAFLUSH, &TermAttributes);
+}
 void AshellPrint(const char* output){
 //Write to 0 for STDIN_FILENO
 //Write to 1 fir STDOUT_FILENO
@@ -308,135 +340,214 @@ char* ff(char* filename){
 //TODO
 };
 
+//convert a string
+//https://www.geeksforgeeks.org/how-to-convert-a-single-character-to-string-in-cpp/
+std::string charString (char x){
+    std::string str(1, x);
+    return str;
+}
+
 int size_of(char *array){
-	int i = 0;
-	while(array[i] != '\0'){
-		i++;
-	}
-	return i;
+    int i = 0;
+    while(array[i] != '\0'){
+        i++;
+    }
+    return i;
 }
 
 void CallPrograms(char **seperated, int num_args){
-	char * run_program = seperated[0];
+    char * run_program = seperated[0];
 
-	if(run_program[0] == 'c' && run_program[1] == 'd' && run_program[2] == '\0'){
-		std::cout <<"Execute CD "<<"\n";
-		char * directory = seperated[1]; //TODO: check to see if it exists
-		cd(directory);
-	}
-	else if(run_program[0] == 'l' && run_program[1] == 's' && run_program[2] == '\0'){
-		std::cout <<"Execute LS "<<"\n";
-		//TODO: ls(directory);
-	}
-	else if(run_program[0] == 'p' && run_program[1] == 'w' && run_program[2] == 'd' && run_program[3] == '\0'){
-		std::cout <<"Execute PWD "<<"\n";
-		pwd();
-	}
-	else if(run_program[0] == 'e' && run_program[1] == 'x' && run_program[2] == 'i' && run_program[3] == 't' && run_program[4] == '\0'){
-		std::cout <<"Execute EXIT "<<"\n";
-		exit();
-	}
-	else if(run_program[0] == 'f' && run_program[1] == 'f' && run_program[2] == '\0'){
-		std::cout <<"Execute ff "<<"\n";
-		char * filename = seperated[1];
-		std::cout <<"Filename:  "<< filename <<"\n";
-		//TODO: ff(filename);
-	}
-	else{
-		std::cout <<"Run Exec(" << run_program<<");" <<"\n";
-		//TODO: exec(program_name)
-	}
+    if(run_program[0] == 'c' && run_program[1] == 'd' && run_program[2] == '\0'){
+        std::cout <<"Execute CD "<<"\n";
+
+        char * directory = seperated[1]; //TODO: check to see if it exists
+        std::cout <<"directory "<< directory <<"\n";
+        //cd(directory);
+    }
+    else if(run_program[0] == 'l' && run_program[1] == 's' && run_program[2] == '\0'){
+        std::cout <<"Execute LS "<<"\n";
+        //TODO: ls(directory);
+    }
+    else if(run_program[0] == 'p' && run_program[1] == 'w' && run_program[2] == 'd' && run_program[3] == '\0'){
+        std::cout <<"Execute PWD "<<"\n";
+        //pwd();
+    }
+    else if(run_program[0] == 'e' && run_program[1] == 'x' && run_program[2] == 'i' && run_program[3] == 't' && run_program[4] == '\0'){
+        std::cout <<"Execute EXIT "<<"\n";
+        exit();
+    }
+    else if(run_program[0] == 'f' && run_program[1] == 'f' && run_program[2] == '\0'){
+        std::cout <<"Execute ff "<<"\n";
+        char * filename = seperated[1];
+        std::cout <<"Filename:  "<< filename <<"\n";
+        //TODO: ff(filename);
+    }
+    else{
+        std::cout <<"Run Exec(" << run_program<<");" <<"\n";
+        //TODO: exec(program_name)
+    }
 
 
 }
 
 void parse(char *prog, char **parsed){
-	//Parsing char array received, basically a split line function.
-	//Currently seperates with ' ' TODO work with any character
+    //Parsing char array received, basically a split line function.
+    //Currently seperates with ' ' TODO work with any character
 
 
-	int i = 0;
-	char split_memory[110];
-	char * split = split_memory;
-	char * seperated[15];
+    int i = 0;
+    char split_memory[110];
+    char * split = split_memory;
+    char * seperated[15];
 
-	//http://www.cplusplus.com/reference/cstring/strtok/
-	split = strtok(prog, " ");
+    //http://www.cplusplus.com/reference/cstring/strtok/
+    split = strtok(prog, " ");
 
-	while (split != NULL){
-		seperated[i] = split;
-		//std::cout <<"seperated[" << i << "] "<< seperated[i] << "\n";
-		parsed[i] = split;
-		//Not sure what split becomes
-		split = strtok(NULL, " ");
-		++i;
-	}
-	seperated[i] = split;
-
-	std::cout << "\n\n";
-
-	char * run_program = seperated[0];
-	int num_args = i -1;
+    while (split != NULL){
+        seperated[i] = split;
+        //std::cout <<"seperated[" << i << "] "<< seperated[i] << "\n";
+        parsed[i] = split;
+        //Not sure what split becomes
+        split = strtok(NULL, " ");
+        ++i;
+    }
+    seperated[i] = split;
 
 
-	CallPrograms(seperated, num_args);
+
+    char * run_program = seperated[0];
+    int num_args = i -1;
+
+
+    CallPrograms(seperated, num_args);
 }
 
 void ReadAndParseCmd() {
 
-	bool end_line = false;
-	int max_size = 512;  //TODO: switch to buffer or malloc system if necessary
-	int i = 0;
+    bool end_line = false;
+    int max_size = 512;  //TODO: switch to buffer or malloc system if necessary
+    int i = 0;
 
-	char test_array[100] = "12345678";
+    char test_array[100] = "12345678";
 
-	size_t bytes_read = 0;
+    size_t bytes_read = 0;
 
-	//if you do this it creates in_one in read-only memory, can't change
-	//char *in_one = "one-";
+    //if you do this it creates in_one in read-only memory, can't change
+    //char *in_one = "one-";
 
-	char prog_mem[100] = "12345678";
-	char args_mem[100] = "12345678";
+    char prog_mem[100] = ""; //should be large?
+    char args_mem[100] = ""; //should b 16 (max can show on terminal)?
 
-	char char_read = '\0';
-	char * prog = prog_mem; //the program contains all input
-	char * parsed; //to contain parsed input
-	char *args = args_mem; //Input after the command/program
+    char char_read = '\0';
+    char * prog = prog_mem; //the program contains all input
+    char * parsed; //to contain parsed input
+    char *args = args_mem; //Input after the command/program
+    bool arrow_flag;
 
-	int max_bytes = 1; //reads at most 1 byte at a time
-	int fd_read = 0; //this if the fd (file descriptor) for read
+    int max_bytes = 1; //reads at most 1 byte at a time
 
-
-
-	while (!end_line){
-		//bytes_read is the number of bytes SUCCESSFULLY read
-		//Example: if "Ben" is typed with max_bytes being 10, 4 is returned.
-		//std::cout <<"got to while" << "\n";
-		bytes_read = read(fd_read, &char_read, max_bytes);
+    //this may need to be STDIN_FILENO
+    int fd_read = 0; //this if the fd (file descriptor) for read
 
 
-		//if the input is readable
-		if (isprint(char_read)){
-			//std::cout <<"got to if" << "\n";
-			//std::cout <<"got to if" << "\n";
-			//std::cout <<"this comes out: " << prog << "\n";
-			prog[i] = char_read;
-		}
+    struct termios SavedTermAttributes; //from noncanmode.c
+    SetNonCanonicalMode(0, &SavedTermAttributes); //from noncanmode.c
 
-		else if ("0\\") {
-			//std::cout <<"got to end line" << "\n";
-			end_line = true;
-			//always a null character at the end of the string
-			prog[i] = '\0';
+    while (!end_line){
+        //bytes_read is the number of bytes SUCCESSFULLY read
+        //Example: if "Ben" is typed with max_bytes being 10, 4 is returned.
+        //std::cout <<"got to while" << "\n";
+        bytes_read = read(STDIN_FILENO, &char_read, max_bytes);
 
-		}
-		//std::cout <<"out of if" << "\n";
-		i++;
 
-	}
-	//std::cout <<"prog:  " << prog << "    args:   " << args<< "\n\n";
-	parse(prog,&args);
-	//std::cout <<"Done."<< "\n";
+        //if the input is readable
+        if(0x04 == char_read){ // C-d
+            std::cout <<"its something else entirely..." << "\n";
+            break;
+        }
+        //BACKSPACE CASE
+        else if (0x7F == char_read){
+            //std::cout <<"got to if" << "\n";
+            //std::cout <<"got to if" << "\n";
+            AshellPrint("BACKSPACE");
+            prog[i] = char_read;
+            //std::cout <<"this comes out: " << prog << "\n";
+
+        }
+        //ARROW CASE
+        else if (arrow_flag && 0x5B != char_read){
+
+            //std::cout <<"ARROW2" << "\n";
+
+
+            if(0x41 == char_read){
+                //UPARROW
+                AshellPrint("UPARROW");
+                arrow_flag = false;
+            }
+            else if(0x42 == char_read){
+                //DOWNARROW
+                AshellPrint("DOWNARROW");
+                arrow_flag = false;
+            }
+            else if(0x43 == char_read){
+                //RIGHTARROW
+                AshellPrint("RIGHTARROW");
+                arrow_flag = false;
+            }
+            else if(0x44 == char_read){
+                //LEFTARROW
+                AshellPrint("LEFTARROW");
+                arrow_flag = false;
+            }
+            else{
+                //ERROR, shouldn't happen
+            }
+
+
+        }
+        else if (arrow_flag && 0x5B == char_read){
+
+            arrow_flag = true;
+
+        }
+
+        else if (isprint(char_read)){
+            //these lines don't fix it
+            prog[i] = char_read;
+            //char dec_to_char = prog[i];
+            //std::cout << "prog[i]: " << prog[i] <<"\n"; //getting the decimal representation
+            std::string str1 = charString(prog[i]);
+            AshellPrint(str1);
+
+        }
+        //ENTER CHASE
+        else if (0x0A == char_read) {
+            std::cout <<"got to end line - so we think 1" << "\n";
+            end_line = true;
+            //always a null character at the end of the string
+            //prog[i] = '\0';
+
+        }
+        else{
+            //std::cout <<"char(" << char_read<<")";
+            //ARROW CASE
+            //AshellPrint("ARROW");
+            arrow_flag = true;
+            //end_line = true;
+            //always a null character at the end of the string
+            //prog[i] = '\0';
+
+        }
+        //std::cout <<"out of if" << "\n";
+        i++;
+
+    }
+    ResetCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
+    //std::cout <<"prog:  " << prog << "    args:   " << args<< "\n\n";
+    parse(prog,&args);
+    //std::cout <<"Done."<< "\n";
 
 
 }
