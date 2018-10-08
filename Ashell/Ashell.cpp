ef0271395c578898f167647f84fff8970ece3923
bllarrison <35443795+bllarrison@users.noreply.github.com>
2018-10-08 11:31:29 -0700

    Added backspace

--- Ashell/Ashell.cpp
+++ Ashell/Ashell.cpp
@@ -477,7 +477,7 @@ void ReadAndParseCmd() {
         else if (0x7F == char_read){
             //std::cout <<"got to if" << "\n";
             //std::cout <<"got to if" << "\n";
-            AshellPrint("\b \b");
+            AshellPrint("\b \b"); //this backspaces
             prog[i] = char_read;
             //std::cout <<"this comes out: " << prog << "\n";
 
